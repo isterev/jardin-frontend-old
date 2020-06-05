55133f096edf8dda358f110093f580b04f9834ca
isterev <isterev@users.noreply.github.com>
2020-06-05 19:03:00 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends React.Component {
         super(props);
 
         this.state = {
-            title: 'Jardin test v1',
+            title: 'Jardin',
             routes: [
                 { component: MarketOfferListView , path: '/', exact: true},
                 { component: MarketOfferDetailView , path: '/show/:id'},
