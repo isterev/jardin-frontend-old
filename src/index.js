d2dd97edebda4f34e85132fb56f5819e486a5e77
isterev <isterev@users.noreply.github.com>
2020-06-07 22:26:08 +0200

    green colors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import WebFontLoader from 'webfontloader';
-import 'react-md/dist/react-md.indigo-pink.min.css'
+import 'react-md/dist/react-md.green-light_green.min.css'
+// alternatives here: 
+// https://unpkg.com/browse/react-md@1.16.1/dist/
+// import 'react-md/dist/react-md.green-lime.min.css'
 
 WebFontLoader.load({
     google: {
